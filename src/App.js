1b795628575d69bb5a9d962e1f55c8013e89ae9d
Abeyy <abey.joy21@gmail.com>
2019-09-02 11:47:07 -0700

    Add Axios, start fetching Dota API

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <card> </card>
+        <Card teamName="CLG"> </Card>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import axios from 'axios';
+import './Card.css'
 
 function Card (props) {
+  async function getDotaTeams () {
+    let response = await axios.get('https://api.opendota.com/api/teams');
+    return response
+  }
+
+  getDotaTeams().then(data => console.log(data))
   return (
-    <h1> Hello World I am a Card </h1>
+    <div className="card-component">
+      <div className="team-logo-container">
+        <img src={props.teamLogo} alt="Team Logo" />
+      </div>
+      <div className="team-metadata-container">
+        <h1>{props.teamName}</h1>
+        <h2>{props.teamSubText}</h2>
+      </div>
+    </div>
   );
 }
 
